93d3b5611c77d73487d2abed1d1988dc970211c8
Ujwal <ujjwalnagrikar@gmail.com>
2025-09-24 18:19:37 +0530

    teat deploy

---
+++ infrastructure/ec2.tf
@@ -0,0 +1,12 @@
+#ec2 instance
+
+resource "aws_instance" "my-server" {
+    ami =  "ami-02d26659fd82cf299"
+    instance_type = "t2.medium"
+    key_name = "Ujwal-DevOps-SRE.pem"
+    subnet_id = aws_subnet.my-public-subnet.id
+    security_groups = [aws_security_group.my-security-group.id]
+    tags = {
+        Name = "my-server"
+    }
+}
