511fc2b293381da9dfaa1dd9fa7741d0df05b01e
Ujwal Nagrikar <98095028+UjwalNagrikar@users.noreply.github.com>
2025-09-26 20:51:00 +0530

    Update ec2.tf

--- infrastructure/ec2.tf
+++ infrastructure/ec2.tf
@@ -3,7 +3,7 @@
 resource "aws_instance" "my-server" {
     ami =  "ami-02d26659fd82cf299"
     instance_type = "t2.medium"
-    key_name = "Ujwal-DevOps-SRE.pem"
+    key_name = "Ujwal-DevOps-SRE"
     subnet_id = aws_subnet.my-public-subnet.id
     security_groups = [aws_security_group.my-security-group.id]
     tags = {
